23accfe7538c25c4ae8c1b3868bd11df31131705
ErissonCalado <74382139+ErissonCalado@users.noreply.github.com>
2020-12-18 17:59:12 -0300

    Add files via upload

---
+++ Feira_Organica_Erisson_Calado.py
@@ -0,0 +1,137 @@
+folhas_e_hortaliças = {'Alface americano': 2.50, 'Alface crespa': 2.50, 'Alho poró': 2.00, 'Capim santo': 2.50, 'Cebola': 3.00, 'Cebolinha': 2.50,
+                       'Coentro': 2.50, 'Couve folha': 2.50, 'Chinguezay (acelga chinesa)': 3.00, 'Espinafre': 3.00, 'Hortelã': 2.50, 'Salsinha': 2.50, 'Rúcula': 2.50}
+
+frutas = {'Banana pacovan | a unidade': 0.25, 'Cana | o saquinho': 2.00,
+          'Laranja comum | 4 unidades': 2.00, 'Laranja mimo |4 unidades': 2.00, 'Maracujá | 1 kg': 7.00}
+
+r_t_l_a = {'Batata doce | 1 kg': 4.00, 'Cará': 5.00, 'Cenoura | o molho': 3.00, 'Jerimum 1 Kg': 5.00,
+           'Macaxeira | 1 Kg': 4.00, 'Rabanete | molho': 2.50, 'Quiabo | 15 unidades': 2.00}
+
+outros = {'Fava seca | 1 Kg': 12.00, 'Mel italiana | 250 gr': 20.00, 'Mel italiana | 500 gr': 35.00, 'Mel no favo | 450 gr': 25.00, 'Ovos de capoeira | a unidade': 1.00, 'Polpa de cajá | 400 gr': 6.00, 'Própolis | 20 ml': 16.00,
+          'Pão sem trigo(grande)': 13.00, 'Pão com trigo(grande)': 13.00, 'Pão com trigo(pequeno)': 7.00, 'Bolo(sem trigo)': 10.00, 'Bolinho de bacia(com trigo)': 2.00, 'Mini pizza | unidade': 3.00, 'Pizza brotinho | unidade': 5.00, 'Bolacha com trigo | saquinho': 5.00, 'Sucos sem açucar | 200 ml': 3.00,
+          'Sucos com açucar | 200 ml': 3.00, 'Sucos com ou sem açucar | 1 litro': 10.00, 'Refeições congeladas | 500 gr': 12.00, 'Refeições congeladas | 750 gr': 15.00, 'Hambúrguer de ora-pro-nóbis | 6 unidades': 12.00, 'Molhos prontos': 10.00, 'Massa artesanal(lasanha ou taglatelle)': 12.00}
+
+past_ant_gel = {'Pepita de girassol': 5.00, 'Homus de grão de bico com paprika': 10.00, 'Bisnaga maionese de pepita girassol | 250 ml': 10.00, 'Pimentas ao mel de engenho': 15.00,
+                'Confit de tomatinho, pimenta, pimentão ou berinjela': 15.00, 'Geleia de tomate com pimenta/abacaxi/manga': 13.00, 'Caponata Siciliana': 13.00}
+
+l_sem_trigo = {'Quiche de macaxeira com alho poró': 5.00, 'Quiche de macaxeira com tomate seco': 5.00,
+               'Sanduiche sem glúten de ricota': 6.00, 'Sanduiche sem glúten de caponata siciliana': 6.00, 'Sanduiche sem glúten de ragu': 6.00}
+
+l_com_trigo = {'Empada de falso camarão': 5.00, 'Empada de antepasto de berinjela': 5.00,
+               'Empada de tofu com cebola caramelizada': 5.00, 'Pãozinhos de inhame recheados': 5.00}
+
+listas = {'0': 'Fim', '1': 'Folhas e Hortaliças', '2': 'Frutas', '3': 'Raízes, Tubérculos, Legumes e Afins',
+          '4': 'Outros', '5': 'Pastinhas, Antepastos e Geleias', '6': 'Lanches sem trigo', '7': 'Lanches com trigo'}
+
+
+todos = {'alface americano': 2.50, 'alface crespa': 2.50, 'alho poró': 2.00, 'capim santo': 2.50, 'cebola': 3.00, 'cebolinha': 2.50,
+         'coentro': 2.50, 'couve folha': 2.50, 'chinguezay ': 3.00, 'espinafre': 3.00, 'hortelã': 2.50, 'salsinha': 2.50, 'rúcula': 2.50, 'banana pacovan': 0.25, 'cana': 2.00,
+         'laranja comum': 2.00, 'laranja mimo': 2.00, 'maracujá': 7.00, 'batata doce': 4.00, 'cará': 5.00, 'cenoura': 3.00, 'jerimum': 5.00,
+         'macaxeira': 4.00, 'rabanete': 2.50, 'quiabo': 2.00, 'fava seca': 12.00, 'mel italiana': 20.00, 'mel italiana': 35.00, 'mel no favo': 25.00, 'ovos de capoeira': 1.00, 'polpa de cajá': 6.00, 'própolis': 16.00,
+         'pão sem trigo(grande)': 13.00, 'pão com trigo(grande)': 13.00, 'pão com trigo(pequeno)': 7.00, 'bolo(sem trigo)': 10.00, 'bolinho de bacia(com trigo)': 2.00, 'mini pizza': 3.00, 'pizza brotinho': 5.00, 'bolacha com trigo': 5.00, 'sucos sem açucar': 3.00,
+         'sucos com açucar': 3.00, 'sucos com ou sem açucar': 10.00, 'refeições congeladas': 12.00, 'refeições congeladas': 15.00, 'hambúrguer de ora-pro-nóbis': 12.00, 'molhos prontos': 10.00, 'massa artesanal(lasanha ou taglatelle)': 12.00,
+         'pepita de girassol': 5.00, 'homus de grão de bico com paprika': 10.00, 'bisnaga maionese de pepita girassol': 10.00, 'pimentas ao mel de engenho': 15.00,
+         'confit de tomatinho, pimenta, pimentão ou berinjela': 15.00, 'geleia de tomate com pimenta/abacaxi/manga': 13.00, 'caponata Siciliana': 13.00, 'quiche de macaxeira com alho poró': 5.00, 'quiche de macaxeira com tomate seco': 5.00,
+         'sanduiche sem glúten de ricota': 6.00, 'sanduiche sem glúten de caponata siciliana': 6.00, 'sanduiche sem glúten de ragu': 6.00, 'empada de falso camarão': 5.00, 'empada de antepasto de berinjela': 5.00,
+         'empada de tofu com cebola caramelizada': 5.00, 'pãozinhos de inhame recheados': 5.00, }
+
+
+def dfolhas_e_hortaliças():
+    print('\n------------Folhas e Hortaliças------------\n')
+    for key, value in folhas_e_hortaliças.items():
+        print('{:<60}'.format(key), '◦◦◦◦◦''       ' 'R$', value)
+
+
+def dfrutas():
+    print('\n------------Frutas------------\n')
+    for key, value in frutas.items():
+        print('{:<60}'.format(key), '◦◦◦◦◦''       ' 'R$', value)
+
+
+def dr_t_l_a():
+    print('\n------------Raízes, Tubérculos, Legumes e Afins-----------------\n')
+    for key, value in r_t_l_a.items():
+        print('{:<60}'.format(key), '◦◦◦◦◦''       ' 'R$', value)
+
+
+def doutros():
+    print('\n------------Outros------------\n')
+    for key, value in outros.items():
+        print('{:<60}'.format(key), '◦◦◦◦◦''       ' 'R$', value)
+
+
+def dpast_ant_gel():
+    print('\n------------Pastinhas, Antepastos e Geleias------------\n')
+    for key, value in past_ant_gel.items():
+        print('{:<60}'.format(key), '◦◦◦◦◦''       ' 'R$', value)
+
+
+def dl_sem_trigo():
+    print('\n------------Lanches sem trigo------------\n')
+    for key, value in l_sem_trigo.items():
+        print('{:<60}'.format(key), '◦◦◦◦◦''       ' 'R$', value)
+
+
+def dl_com_trigo():
+    print('\n------------Lanches com trigo------------\n')
+    for key, value in l_com_trigo.items():
+        print('{:<60}'.format(key), '◦◦◦◦◦''       ' 'R$', value)
+
+
+compras = []
+total_compra = 0.0
+
+
+def listagem_exe():
+    dfolhas_e_hortaliças()
+    dfrutas()
+    dr_t_l_a()
+    doutros()
+    dpast_ant_gel()
+    dl_sem_trigo()
+    dl_com_trigo()
+
+
+print(listagem_exe())
+registrando = True
+while(registrando):
+    codigo = str(
+        input('informe o nome do item(como está escrito) ou SAIR para finalizar: '))
+    codigo = codigo.lower()
+    if codigo == "sair":
+        registrando = False
+    else:
+        if(codigo in todos):
+            quantidade = int(input('forneça a quantidade: '))
+            compras.append({
+                "item": codigo,
+                "quantidade": quantidade
+            })
+        else:
+            print('produto não existe, retornando\n\n')
+
+print('\n\nfim da compra\n')
+
+for i, compra in enumerate(compras):
+
+    codigo = compra['item']
+    quantidade = compra['quantidade']
+    preco = todos[codigo]
+
+    subtotal = preco * quantidade
+    total_compra = total_compra + subtotal
+
+    print('item:', codigo, '  valor unitário:', preco,
+          '  quantidade:', quantidade, '  subtotal:', subtotal)
+
+print('\ntotal da compra:', total_compra)
+
+nome = input(str('Informe o seu nome:\n'))
+endereço = input(str('Informe o seu endereço:\n'))
+pagamento = input(str('Informe a forma de pagamento:\n'))
+
+with open('compras.txt', 'w', encoding='utf8') as arquivo:
+    arquivo.write('\nNome:'+nome)
+    arquivo.write('\nEndereço:'+endereço)
+    arquivo.write('\nForma de pagamento:'+pagamento)
+    arquivo.write('\nTotal do pedido:'+str(total_compra))
